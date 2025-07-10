6072f44a0708844e23ce6527b2ef3bf3d983962b
Nima Makhmali <nmkhmly790@gmail.com>
2025-07-10 03:54:19 -0700

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module sinar_chain
 
 go 1.24.3
+
+replace gopkg.in/urfave/cli.v1 => github.com/urfave/cli v1.20.0
+
+replace github.com/ethereum/go-ethereum => github.com/Fantom-foundation/go-ethereum v1.9.22-ftm-0.5
--- src/event/event.go
+++ src/event/event.go
@@ -1,18 +1,16 @@
 package core
 
+
 import (
-	"bytes"
-	"time"
 	"crypto/ecdsa"
 	"fmt"
 	"sync/atomic"
+	
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/rlp"
 	"golang.org/x/crypto/sha3"
-	"github.com/ethereum/go-ethereum/core/types"
-
 )
-
 // Event Id is a 31-byte hash of the event
 type EventID [32]byte
 
@@ -27,24 +25,23 @@ type transaction struct {
 // EventHeader contains metadata of the event
 type EventHeader struct {
 	//ID            string
-	CreatorID     string        // ID or Address of the node
-	Parents       []EventID     // references to parent events
-	Lamport       uint64        // Lamport timestamp (logical clock)
-	Epoch         uint64        // epoch number
-	ExtraData     []byte        // custom extra info
-	TxHash        [32]byte        // hash of transactions
+	CreatorID     string    // ID or Address of the node
+	Parents       []EventID // references to parent events
+	Lamport       uint64    // Lamport timestamp (logical clock)
+	Epoch         uint64    // epoch number
+	ExtraData     []byte    // custom extra info
+	TxHash        [32]byte  // hash of transactions
 	TxCount       int
-	Round         uint64        // برای الگوریتم اجماع DAG
-	Frame         uint64        // (optional) برای دسته‌بندی منطقی
-	Height        uint64        // بیشینه‌ی (height والدها) + 1
-	RoundReceived uint64        // dar kodam round be ejmae reside?
-	IsFamous      *bool         // is famous ?
-	Atropos       EventID       // hash event nahaii ke ino taeed karde
-	AtroposTime   uint64        // zaman ejmae nahaii
-	MedianTime    uint64        // Average time of dag
-	hash          atomic.Value  // cached hash
+	Round         uint64       // برای الگوریتم اجماع DAG
+	Frame         uint64       // (optional) برای دسته‌بندی منطقی
+	Height        uint64       // بیشینه‌ی (height والدها) + 1
+	RoundReceived uint64       // dar kodam round be ejmae reside?
+	IsFamous      *bool        // is famous ?
+	Atropos       EventID      // hash event nahaii ke ino taeed karde
+	AtroposTime   uint64       // zaman ejmae nahaii
+	MedianTime    uint64       // Average time of dag
+	hash          atomic.Value // cached hash
 }
-
 
 // Event is a full structure containing all necessary data for DAG
 type Event struct {
@@ -62,21 +59,44 @@ type rlpEvent struct {
 	Payload      [][]byte
 }
 
+type DAG struct {
+	Events map[EventID]*Event
+}
 
-func NewEvent(creatorID string, parents []EventID, epoch uint64, lamport uint64, txs []transaction) *Event {
+func NewEvent(creatorID string, parents []EventID, epoch uint64, lamport uint64, txs []transaction, dag *DAG) *Event {
 	height := uint64(0)
-	return &Event {
-		EventHeader: EventHeader{
-			CreatorID:  creatorID,
-			Parents:  parents,
-			Epoch:    epoch,
-			Lamport:  lamport,
-			TxCount:  len(txs),
-			Height: height,
-		},
-		Transactions: txs,
-		Signature:    nil,
+	for _, parentID := range parents {
+		if parent, exists := dag.Events[parentID]; exists {
+			if parent.Height > height {
+				height = parent.Height
+			}
+		}
 	}
+	height += 1
+	e := &Event{
+		EventHeader: EventHeader{
+			CreatorID: creatorID,
+			Parents:   parents,
+			Epoch:     epoch,
+			Lamport:   lamport,
+			TxCount:   len(txs),
+			Height:    height,
+		},
+	}
+	e.EventHeader.TxHash = e.CalculateTxHash()
+	return e
+}
+
+func (e *Event) CalculateTxHash() [32]byte {
+	hasher := sha3.NewLegacyKeccak256()
+	txsBytes, err := rlp.EncodeToBytes(e.Transactions)
+	if err != nil {
+		panic(fmt.Errorf("Failed to encode transactions: %v", err))
+	}
+	hasher.Write(txsBytes)
+	var txHash [32]byte
+	copy(txHash[:], hasher.Sum(nil))
+	return txHash
 }
 
 func (e *Event) Hash() EventID {
@@ -110,7 +130,6 @@ func (e *Event) DataToSign() []byte {
 	hash := e.Hash()
 	return hash[:]
 }
-
 
 func (e *Event) Sign(priv *ecdsa.PrivateKey) error {
 
